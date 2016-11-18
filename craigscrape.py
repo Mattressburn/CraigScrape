ef9986aea9c0b27519eae5460fe3f49c2c483015
Matthew <Matthew@USERs-MacBook-Pro.local>
2016-11-17 22:37:43 -0500

    loops through search

--- craigscrape.py
+++ craigscrape.py
@@ -20,35 +20,35 @@ html = BeautifulSoup(rsp.text, 'html.parser')
 apts = html.find_all('p', attrs={'class': 'result-info'})
 print(len(apts))
 
-this_appt = apts[0]
-print(this_appt.prettify())
+for this_appt in apts:
+    
 
-size = this_appt.findAll(attrs={'class': 'housing'})[0].text
-print(size)
+        size = this_appt.findAll(attrs={'class': 'housing'})[0].text
+        print(size)
 
-def find_size_and_brs(size):
-    split = size.strip(' /- \n').split(' -\n')
-    if len(split) == 2:
-        n_brs = split[0].replace('br', '')
-        this_size = split[1].replace('ft2', '')
-    elif 'br' in split[0]:
-        # It's the n_bedrooms
-        n_brs = split[0].replace('br', '')
-        this_size = np.nan
-    elif 'ft2' in split[0]:
-        # It's the size
-        this_size = split[0].replace('ft2', '')
-        n_brs = np.nan
-    return float(this_size), float(n_brs)
-this_size, n_brs = find_size_and_brs(size)
+        def find_size_and_brs(size):
+            split = size.strip(' /- \n').split(' -\n')
+            if len(split) == 2:
+                n_brs = split[0].replace('br', '')
+                this_size = split[1].replace('ft2', '')
+            elif 'br' in split[0]:
+                # It's the n_bedrooms
+                n_brs = split[0].replace('br', '')
+                this_size = np.nan
+            elif 'ft2' in split[0]:
+                # It's the size
+                this_size = split[0].replace('ft2', '')
+                n_brs = np.nan
+            return float(this_size), float(n_brs)
+        this_size, n_brs = find_size_and_brs(size)
 
 
-this_time = this_appt.find('time')['datetime']
-this_time = pd.to_datetime(this_time)
-this_price = float(this_appt.find('span', {'class': 'result-price'}).text.strip('$'))
-this_title = this_appt.find('a', attrs={'class': 'hdrlnk'}).text
+        this_time = this_appt.find('time')['datetime']
+        this_time = pd.to_datetime(this_time)
+        this_price = float(this_appt.find('span', {'class': 'result-price'}).text.strip('$'))
+        this_title = this_appt.find('a', attrs={'class': 'hdrlnk'}).text
 
-print(this_time, this_price, this_title)
+        print(this_time, this_price, this_title)
 
 
 
